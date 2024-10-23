dc6d300b6c2ded56d66973df399372be5255c51c
DiegoSHS <diegoawesome40@gmail.com>
2024-10-23 17:59:10 -0600

    Added stacks to layout

--- components/features/programDirector/application/screens/homeView.tsx
+++ components/features/programDirector/application/screens/homeView.tsx
@@ -1,112 +1,69 @@
-import { Link } from "expo-router";
-import { StyleSheet, View, TouchableOpacity } from "react-native";
+import { Link, router } from "expo-router";
+import { StyleSheet } from "react-native";
 import { Button } from "react-native-paper";
 import { globalStyles } from "../../../auth/application/screens/loginView";
 import { ThemedView } from "@/components/ThemedView";
 
 export default function HomeView() {
+  const goCreatePartial = () => router.push('/programDirector/createPartial')
+  const goApprovedTemplate = () => router.push('/programDirector/approvedTemplate')
+  const goPendingTemplate = () => router.push('/programDirector/pendingTemplate')
+  const goCorrectedTemplate = () => router.push('/programDirector/correctedTemplate')
+  const goTeachingStaff = () => router.push('/programDirector/teachingStaff')
+  const goBack = () => router.push('/auth/login')
   return (
     <ThemedView style={globalStyles.container}>
-        <Button
-          buttonColor="#309b7b"
-          textColor="white"
-          style={globalStyles.button}
-        >
-          <Link href={"/programDirector/createPartial"}>
-            Crear Plantilla Parcial
-          </Link>
-        </Button>
-        <Button
-          buttonColor="#309b7b"
-          style={globalStyles.button}
-          textColor="white"
-        >
-          <Link href={"/programDirector/approvedTemplate"}>
-            Ver Plantillas Parciales Aprobadas
-          </Link>
-        </Button>
-        <Button
-          buttonColor="#309b7b"
-          style={globalStyles.button}
-          textColor="white"
-        >
-          <Link href={"/programDirector/pendingTemplate"}>
-            Ver Plantillas Parciales Pendientes
-          </Link>
-        </Button>
+      <Button
+        onPress={goCreatePartial}
+        buttonColor="#309b7b"
+        textColor="white"
+        style={globalStyles.button}
+      >
+        Crear Plantilla Parcial
+      </Button>
+      <Button
+        onPress={goApprovedTemplate}
+        buttonColor="#309b7b"
+        style={globalStyles.button}
+        textColor="white"
+      >
+          Ver Plantillas Parciales Aprobadas
+      </Button>
+      <Button
+        onPress={goPendingTemplate}
+        buttonColor="#309b7b"
+        style={globalStyles.button}
+        textColor="white"
+      >
+          Ver Plantillas Parciales Pendientes
+      </Button>
+      <Button
+        onPress={goCorrectedTemplate}
+        buttonColor="#309b7b"
+        style={globalStyles.button}
+        textColor="white"
+      >
+        <Link href={"/programDirector/correctedTemplate"}>
+          Ver Plantillas Parciales Por Corregir
+        </Link>
+      </Button>
 
-        <Button
-          buttonColor="#309b7b"
-          style={globalStyles.button}
-          textColor="white"
-        >
-
-          <Link href={"/programDirector/pendingTemplate"}>
-            Ver Plantillas Parciales Pendientes
-          </Link>
-
-        </Button>
-        <Button
-          buttonColor="#309b7b"
-          style={globalStyles.button}
-          textColor="white"
-        >
-          <Link href={"/programDirector/correctedTemplate"}>
-            Ver Plantillas Parciales Por Corregir
-          </Link>
-        </Button>
-
-        <Button
-          buttonColor="#309b7b"
-          style={globalStyles.button}
-          textColor="white"
-        >
-          <Link href={"/programDirector/teachingStaff"}>
-            Plantillas Docentes
-          </Link>
-        </Button>
-        <Button
-          buttonColor="#309b7b"
-          style={globalStyles.button}
-          textColor="white"
-        >
-          <Link href={"/auth/login"}>
-            Salir
-          </Link>
-        </Button>
+      <Button
+        onPress={goTeachingStaff}
+        buttonColor="#309b7b"
+        style={globalStyles.button}
+        textColor="white"
+      >
+          Plantillas Docentes
+      </Button>
+      <Button
+        onPress={goBack}
+        buttonColor="#EB1414FF"
+        style={globalStyles.button}
+        textColor="white"
+      >
+          Salir
+      </Button>
     </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#F5F5F5',
-  },
-  button: {
-    width: '100%',
-    backgroundColor: '#32CD32',
-    borderRadius: 10,
-    paddingVertical: 15,
-    marginVertical: 10,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 16,
-  },
-  logoutButton: {
-    width: '100%',
-    backgroundColor: '#FF6347',
-    borderRadius: 10,
-    paddingVertical: 15,
-    marginVertical: 10,
-    alignItems: 'center',
-  },
-  logoutButtonText: {
-    color: '#FFFFFF',
-    fontSize: 16,
-  },
-});
