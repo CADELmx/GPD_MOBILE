1f0d981bf766f27c49a294df46a032c8e76132e6
Elisa <113410035+elisa242003@users.noreply.github.com>
2024-11-04 22:31:09 -0600

    Feature 05/users (#10)
    * Creating folders
    
    * Full CRUD of areas
    
    * home Subjects
    
    * Interfaces
    
    * Replaced TouchableOpacity with Button component
    
    * interfaces educational Programs
    
    * Deleted nested layouts and replaced links with router
    
    - replaced links with expo router
    - deleted many _layout files
    
    * styles
    
    * rutas
    
    * Added stacks to layout
    
    * CRUD Subjects
    
    * route modification
    
    * Changes in routes
    
    * View Teaching Templates
    
    * View Users
    
    ---------
    
    Co-authored-by: Ana202016 <122801690+Ana202016@users.noreply.github.com>
    Co-authored-by: DiegoSHS <diegoawesome40@gmail.com>
    Co-authored-by: Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>

---
+++ app/academicSecretary/users/home.tsx
@@ -0,0 +1,8 @@
+import UsersHomeView from "../../../components/features/academicSecretary/users/application/screens/homeView";
+
+export default function UsersHomeScreen(){
+  return(
+    <UsersHomeView/>
+      
+  )
+}
